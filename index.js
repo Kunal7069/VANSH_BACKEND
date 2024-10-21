f0e87ce7e66783447fae78c38074025445207a95
Kunal7069 <jaink7069@gmail.com>
2024-10-22 00:20:21 +0530

    DONE

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const WeatherRoutes = require('./routes/WeatherRoutes');
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+app.get('/', (req, res) => {
+  res.send('Backend is activated');
+});
+app.use('/api', WeatherRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
