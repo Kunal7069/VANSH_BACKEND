8a1ed978227a59cbadf455c3020e7635bc9935c1
Kunal7069 <jaink7069@gmail.com>
2024-10-26 04:18:25 +0530

    done

--- routes/WeatherRoutes.js
+++ routes/WeatherRoutes.js
@@ -48,7 +48,7 @@ async function SearchRolledData(city){
   const today = new Date();
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so add 1
-  const day = String(today.getDate()).padStart(2, '0');
+  const day = String(today.getDate()+1).padStart(2, '0');
   const formattedDate = `${year}-${month}-${day}`;
   const weatherData = await getWeather(city);
   console.log(city)
